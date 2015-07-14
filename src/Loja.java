1dc17076d3c266140dba198007aafcf39e4ab0a9
Marlon Andrel Coelho Freitas <marlonmacf@gmail.com>
2015-07-14 11:56:07 -0300

    Loja 2015.05.02

--- src/Loja.java
+++ src/Loja.java
@@ -1,14 +1,22 @@
-/**
- *
- * @author Márlon Ândrel Coelho Freitas
- * @see Classe principal do projeto
- */
+
+import br.com.views.telas.TelaPrincipal;
+
+
 public class Loja {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        //Seleciona o Windows look and feel
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+        telaPrincipal.setVisible(true);
     }
-
 }
---
+++ src/br/com/models/util/HibernateUtil.java
@@ -0,0 +1,39 @@
+package br.com.models.util;
+
+import static br.com.models.util.HibernateUtil.getSessionFactory;
+import org.hibernate.Session;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+public class HibernateUtil {
+
+    private static Session session;
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    
+    public static Session getSession() {
+        if(session != null && !session.isOpen()){
+            setSessao(null);
+        }
+        if(session == null){
+            session = getSessionFactory().openSession();
+        }
+        return session;
+    }
+
+    public static void setSessao(Session sessao) {
+        HibernateUtil.session = sessao;
+    }
+}
