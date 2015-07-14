1bd6316100e7939e15282a0f11fb1cf10bded795
Marlon Andrel Coelho Freitas <marlonmacf@gmail.com>
2015-07-14 11:51:13 -0300

    Loja 2015.05.01

---
+++ src/Loja.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Márlon Ândrel Coelho Freitas
+ * @see Classe principal do projeto
+ */
+public class Loja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
